ada63f378c89be95b88a0adf150adaa0a94fbef6
卜轩 <202200300410@mail.sdu.edu.cn>
2023-10-09 09:13:44 +0800

    feat:添加新目录功能

--- src/main/java/webcurriculumdesign/backend/controller/BaseController.java
+++ src/main/java/webcurriculumdesign/backend/controller/BaseController.java
@@ -3,6 +3,7 @@ package webcurriculumdesign.backend.controller;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import webcurriculumdesign.backend.annotation.RequiredLogin;
 import webcurriculumdesign.backend.data.vo.Result;
@@ -22,5 +23,19 @@ public class BaseController {
     @PostMapping("/getMainMenu")
     public Result getMainMenu() {
         return basicService.getMainMenu();
+    }
+
+    /**
+     * 添加新的目录
+     *
+     * @param name 目录名称
+     * @param role 用户组
+     * @param route 目录路径
+     * @param parent_id 父节点
+     */
+    @RequiredLogin(roles = "ALL")
+    @PostMapping("/addMenu")
+    public Result addMenu(@RequestParam String name, @RequestParam Integer parent_id, @RequestParam String role, @RequestParam String route) {
+        return basicService.addMenu(name, parent_id, role, route);
     }
 }
--- src/main/java/webcurriculumdesign/backend/service/BasicService.java
+++ src/main/java/webcurriculumdesign/backend/service/BasicService.java
@@ -1,7 +1,9 @@
 package webcurriculumdesign.backend.service;
 
 import jakarta.annotation.Resource;
+import org.apache.catalina.connector.Response;
 import org.springframework.stereotype.Service;
+import webcurriculumdesign.backend.data.enums.Role;
 import webcurriculumdesign.backend.data.po.MainMenu;
 import webcurriculumdesign.backend.data.pojo.CurrentUser;
 import webcurriculumdesign.backend.mapper.MainMenuMapper;
@@ -18,7 +20,31 @@ public class BasicService {
     //获取主目录
     public Result getMainMenu() {
         List<MainMenu> menuList = mainMenuMapper.selectMenuTree(CurrentUser.role);
-        List<MainMenu> treeList = CourseUtil.buildMenuTree(menuList, null);
+        List<MainMenu> treeList = CourseUtil.buildMenuTree(menuList, 0);
         return Result.success(treeList);
+    }
+
+    //添加新目录
+    public Result addMenu(String name, Integer parent_id, String role, String route) {
+        //判断用户组（role）正确性
+        if (!role.equals(Role.ADMIN) && !role.equals(Role.STUDENT) && !role.equals(Role.TEACHER)) {
+            return Result.error(Response.SC_BAD_REQUEST, "用户组错误");
+        }
+
+        List<MainMenu> menuList = mainMenuMapper.selectMenuTree(role);
+
+        //判断名称是否重复
+        for (MainMenu list : menuList) {
+            if (list.getName().equals(name) || list.getRoute().equals(route)) return Result.error(Response.SC_BAD_REQUEST, "名称重复");
+        }
+
+        try {
+            mainMenuMapper.insert(new MainMenu(null, name, parent_id, role, route, null));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Result.error(Response.SC_INTERNAL_SERVER_ERROR, "服务器错误");
+        }
+
+        return Result.ok();
     }
 }
