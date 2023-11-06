2d9e86ff81fe13784e02815a49a8c62b8afe7fac
卜轩 <202200300410@mail.sdu.edu.cn>
2023-11-06 17:09:22 +0800

    fix:修改审查返回

--- web-user/src/main/java/webcurriculumdesign/backend/controller/StudentController.java
+++ web-user/src/main/java/webcurriculumdesign/backend/controller/StudentController.java
@@ -5,19 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import webcurriculumdesign.backend.annotation.RequiredLogin;
-import webcurriculumdesign.backend.data.pojo.CurrentUser;
 import webcurriculumdesign.backend.data.vo.Result;
-import webcurriculumdesign.backend.mapper.AuditMapper;
+import webcurriculumdesign.backend.service.StudentService;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
     @Resource
-    AuditMapper auditMapper;
+    StudentService studentService;
 
     @RequiredLogin(roles = "ALL")
     @GetMapping("/getAuditResult")
     public Result getAuditResult() {
-        return Result.success(auditMapper.getAuditByApplicantId(CurrentUser.id));
+        return studentService.getAuditResult();
     }
 }
