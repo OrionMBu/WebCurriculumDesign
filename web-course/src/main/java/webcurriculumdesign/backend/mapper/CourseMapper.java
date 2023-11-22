13f19c877043a08a602d45e19ec68a7ef2c204f8
Excarii <buxuan_1601@163.com>
2023-11-22 22:35:41 +0800

    feat:添加注释

--- web-course/src/main/java/webcurriculumdesign/backend/mapper/CourseMapper.java
+++ web-course/src/main/java/webcurriculumdesign/backend/mapper/CourseMapper.java
@@ -9,7 +9,16 @@ import java.util.List;
 
 @Mapper
 public interface CourseMapper extends BaseMapper<Course> {
-    // 通过教师姓名等信息查询课程
+    /**
+     * 通过教师姓名等信息查询课程
+     *
+     * @param courseName 课程名称
+     * @param index 课序号
+     * @param teacherName 教师姓名
+     * @param type 课程类型
+     * @param rowBounds 分页设置 new RowBounds(offset, pageSize)
+     * @return List<Course>
+     */
     @Select("SELECT course.*, info_academy.name AS academy " +
             "FROM course_teacher " +
             "INNER JOIN course ON course_teacher.course_id = course.id " +
@@ -18,14 +27,27 @@ public interface CourseMapper extends BaseMapper<Course> {
             "WHERE course.name like '${courseName}' AND course.`index` like '${index}' AND info_teacher.name like '${teacherName}' AND course.type like '${type}'")
     List<Course> searchCourseWithTeacherName(@Param("courseName")String courseName, @Param("index")String index, @Param("teacherName")String teacherName, @Param("type")String type, RowBounds rowBounds);
 
-    // 查询课程
+    /**
+     * 查询课程
+     *
+     * @param courseName 课程名称
+     * @param index 课序号
+     * @param type 课程类型
+     * @param rowBounds 分页设置 new RowBounds(offset, pageSize)
+     * @return List<Course>
+     */
     @Select("SELECT course.*, info_academy.name AS academy " +
             "FROM course " +
             "JOIN info_academy ON info_academy.id = course.academy_id " +
             "WHERE course.name like '${courseName}' AND course.`index` like '${index}' AND course.type like '${type}'")
     List<Course> searchCourseWithoutTeacher(@Param("courseName")String courseName, @Param("index")String index, @Param("type")String type, RowBounds rowBounds);
 
-    // 查询教师课程信息
+    /**
+     * 查询教师课程信息
+     *
+     * @param userId 用户id
+     * @return List<Course>
+     */
     @Select("SELECT course.*, info_academy.name AS academy " +
             "FROM course_teacher " +
             "LEFT JOIN course ON course_teacher.course_id = course.id " +
@@ -33,7 +55,12 @@ public interface CourseMapper extends BaseMapper<Course> {
             "WHERE course_teacher.user_id = #{userId}")
     List<Course> getTeacherCourse(Integer userId);
 
-    // 查询学生课程信息
+    /**
+     * 查询学生课程信息
+     *
+     * @param userId 用户id
+     * @return List<Course>
+     */
     @Select("SELECT course.*, info_academy.name AS academy " +
             "FROM course_student " +
             "LEFT JOIN course ON course_student.course_id = course.id " +
